b7cd58e2b1bb87b8920dfcb3b24c163abe4010b4
Petar Petrov <petar.petrov.georgiev@gmail.com>
2018-11-19 23:02:49 +0100

    Add (test) shields animation

--- src/gfx/index.js
+++ src/gfx/index.js
@@ -1,7 +1,10 @@
 // gfx.js - common graphic effects
 import Globals from '../globals';
 
-const EXPLOSION_ANIM_DURATION = 1200;
+const DURATIONS = {
+  explosion: 1200,
+  shields: 1500
+};
 
 const EXPLOSIONS = {
   'explosion-1': { atlas: Globals.atlas_px },
@@ -15,7 +18,7 @@ class Gfx {
     this.scene = scene;
     this.audio = audio;
     
-    this.addExplosions();
+    this.addAnimations();
     this.bindEvents();
   }
 
@@ -23,23 +26,49 @@ class Gfx {
     this.scene.events.on('explosion', (config) => {
       this.scene.add.sprite(config.x, config.y, 
         EXPLOSIONS[config.name].atlas).play(config.name);
-        // TODO play sfx
+      // play sfx
       this.audio.playSound('explosions');
+    });
+
+    this.scene.events.on('shields', (config) => {
+      const anim = this.scene.add.sprite(config.x, config.y, Globals.atlas1);
+      if (config.cb) {
+        anim.on('animationcomplete', config.cb)
+      }
+      anim.play('shields', true, 0);
+      // TODO play sfx
     });
   }
 
-  addExplosions() {
+  createAnim(sprite, visible = false) {
+    const anim = this.scene.add.sprite(sprite.x, sprite.y, Globals.atlas1);
+    anim.visible = true;
+    return anim;
+  }
+
+  addAnimations() {
     const { scene } = this;
+
     Object.keys(EXPLOSIONS).map(key => {
       scene.anims.create({
         key: key,
         frames: scene.anims.generateFrameNames(EXPLOSIONS[key].atlas),
-        duration: EXPLOSION_ANIM_DURATION,
-        repeat: 0
+        duration: DURATIONS.explosion,
+        repeat: 0,
+        hideOnComplete: true
       });
     });
     // console.log(this.scene.anims.generateFrameNames(Globals.atlas_px,
     //   { end: 8, prefix: 'pixelExplosion', suffix: '.png', zeroPad: 2}));
+
+    scene.anims.create({
+      key: 'shields',
+      frames: scene.anims.generateFrameNames(Globals.atlas1, {
+        start: 1, end: 3, prefix: 'wingBlue_', suffix: '.png'}),
+      frameRate: 30, //DURATIONS.shields,
+      repeat: -1,
+      hideOnComplete: false
+    });
   }
 
 }
